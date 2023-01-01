641978ad2908372eecf250683e1aaf42e74a70e4
Daniel <daniel@grundel.me>
2023-01-01 14:08:30 -0800

    clean dist on build

--- dist/commands/echo.js
+++
@@ -1,9 +0,0 @@
-export class EchoExecutor {
-    constructor() {
-        this.shortDescription = 'Say something.';
-    }
-    invoke(context) {
-        context.cli.println(context.args);
-        return 0;
-    }
-}
--- dist/commands/potato.js
+++
@@ -1,9 +0,0 @@
-export class PotatoExecutor {
-    constructor() {
-        this.shortDescription = 'Print a cute, little potato.';
-    }
-    invoke(context) {
-        context.cli.println('🥔');
-        return 0;
-    }
-}
